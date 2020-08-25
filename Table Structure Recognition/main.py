ceba895be191718d53ed34a9c50cdeace1e9175c
infinity73 <usamabilal73@hotmail.com>
2020-08-25 20:18:06 +0500

    Update main.py

--- Table Structure Recognition/main.py
+++ Table Structure Recognition/main.py
@@ -45,15 +45,15 @@ for i in imgs:
         ## call border script for each table in image
         for res in res_border:
             try:
-                root.append(border(res,plt.imread(i)))
-                #root.append(border(res,cv2.imread(i)))  
+                #root.append(border(res,plt.imread(i)))
+                root.append(border(res,cv2.imread(i)))  
             except:
                 pass
     if len(res_bless) != 0:
         if len(res_cell) != 0:
             for no,res in enumerate(res_bless):
-                root.append(borderless(res,plt.imread(i),res_cell))
-                #root.append(borderless(res,cv2.imread(i),res_cell))
+                #root.append(borderless(res,plt.imread(i),res_cell))
+                root.append(borderless(res,cv2.imread(i),res_cell))
 
     myfile = open(xmlPath+i.split('/')[-1][:-3]+'xml', "w")
     myfile.write('<?xml version="1.0" encoding="UTF-8"?>\n')
